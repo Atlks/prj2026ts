85fe7267e3a0bfed8765738b861a09305a13fa77
dms <dms@uke>
2025-06-18 00:30:10 +0700

    。。。

--- spel.ts
+++ spel.ts
@@ -1,7 +1,9 @@
-// npm i --save-dev @types/spel2js
-import { SpelExpressionEvaluator } from 'spel2js';
+// // npm i --save-dev @types/spel2js
+// import { SpelExpressionEvaluator } from 'spel2js';
 
-const expression = '#user.age > 18';
-const context = { user: { age: 25 } };
+// const expression = '#user.age > 18';
+// const context = { user: { age: 25 } };
 
-console.log(SpelExpressionEvaluator.eval(expression, context)); // true
+// console.log(SpelExpressionEvaluator.eval(expression, context)); // true
+
+//cant find md spel2js
